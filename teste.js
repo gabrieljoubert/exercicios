5954cd6b7bcb65c55196f7e31dff03a419851ad8
gabrieljoubert <112589950+gabrieljoubert@users.noreply.github.com>
2022-11-11 11:43:35 -0300

    first commit

---
+++ teste.js
@@ -0,0 +1,8 @@
+// let = estou declarando a variavel, no java normal se usa var, ja no javascript usa let.
+let num1 = 4;
+let num2 = 4;
+
+console.log('Soma: 4 + 4 = ' + (num1 + num2));
+console.log('Subtração: 4 - 4 = ' + (num1 - num2));
+console.log('Divisão: 4 / 4 = ' + (num1 / num2));
+console.log('Multiplicação: 4 * 4 = ' + (num1 * num2));
